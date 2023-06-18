4f6dace4759871b5ad18e158e3198ae4168c1e7b
Uchral <uchbatkhuyag@gmail.com>
2023-06-18 12:58:11 -0700

    assignment00 started

--- assignment00/assignment00.h
+++ assignment00/assignment00.h
@@ -27,11 +27,33 @@ void string_remove_char(char* s, char x);
 
 
 int string_cmp(const char* s1, const char* s2){
-    return 0;
+    while (*s1 == *s2) {
+        if (*s1 != '\0' && *s2 != '\0'){
+            s1++;
+            s2++;
+        }
+        else {
+            break;
+        }
+    } 
+    if (*s1 > *s2){
+        return 1;
+    }
+    else if (*s1 < *s2) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
 }
 
 size_t string_length(char* s){
-    return 0;
+    size_t size = 0;
+    while (*s != '\0'){
+        size += 1;
+        s++;
+    }
+    return size;
 }
 
 void string_reverse(char* s){
