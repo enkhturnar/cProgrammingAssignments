32023bf2f1992177fc246168cea419714b5255bf
Enkh Nar <enkhtur.nar@gmail.com>
2023-06-27 21:04:43 -0700

    bitwise and testcase improve

--- assignment01/test.c
+++ assignment01/test.c
@@ -195,6 +195,14 @@ void test_bitwiseAND() {
     {
         assert_eq_int(bitwiseAND(-2, 1), 0, "bitwise and incorrect");
     }
+    {
+        for (int i = -1000; i <= 1000; i++) {
+            for (int j = -1000; j <= 1000; j++) {
+                assert_eq_int(bitwiseAND(i, j), i & j, "bitwise and incorrect");
+            }
+        }
+    }
+
     printf("test_bitwiseAND PASS!\n");
 }
 
